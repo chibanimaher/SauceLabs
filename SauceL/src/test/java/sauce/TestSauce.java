1ac5d910dad165f1754af2d81d88ed06c56dcbd2
chibanimaher <chibanimaher@gmail.com>
2019-03-20 18:16:47 +0100

    fincommit

--- SauceL/src/test/java/sauce/TestSauce.java
+++ SauceL/src/test/java/sauce/TestSauce.java
@@ -13,6 +13,7 @@ import org.testng.Assert;
 
 public class TestSauce {
 	 public static WebDriver driver;
+	 static String buildnumber=" 2589 4b3da11";
 	 static String sauceUserName = "Peter.xu";
      static String sauceAccessKey ="cd64b636-96cb-4279-baec-133a16798773";
      static String sauceURL = "https://" + sauceUserName +":"+sauceAccessKey+"@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
@@ -23,7 +24,7 @@ public class TestSauce {
 		  capabilities.setCapability("browserName", "Chrome");
 		 capabilities.setCapability("platform", "Windows 10");
 	     capabilities.setCapability("version", "latest");
-	     capabilities.setCapability("build", System.getenv(" Sauce Connect 4.5.3") + "__" + System.getenv("2589 4b3da11"));
+	     capabilities.setCapability("build", "buildnumber");
 	    driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
 	     //driver =new ChromeDriver();
 	     driver.get("https://google.com");
